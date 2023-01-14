66f4f5df93568b1ebda5dc6a15c23394bb5707ff
Vepp <vinipontarolla@gmail.com>
2023-01-14 23:09:10 +0100

    Add click outside toggle hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,13 @@ import { useCurrentUser,
 useSetCurrentUser} from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useCLickOutsideToggle from "../hooks/useCLickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useCLickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -83,7 +86,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -91,7 +94,10 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
           {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+        ref={ref}
+        onClick={() => setExpanded(!expanded)} 
+        aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
